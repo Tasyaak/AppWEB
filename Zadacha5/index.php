3960837b8230c2b662b4a2925974402d0c272a39
Oleg <61349805+Tasyaak@users.noreply.github.com>
2024-05-07 22:40:59 +0300

    Add files via upload

--- Zadacha5/index.php
+++ Zadacha5/index.php
@@ -1,12 +1,12 @@
 <?php
 
-// $db;
+$db;
 // include('database.php');
-// header("Content Type: text/html, charset-UTF-8");
-// session_start();
+header("Content Type: text/html, charset-UTF-8");
+session_start();
 
-// $error = false;
-// $log = !empty($_SESSION['login']);
+$error = false;
+$log = !empty($_SESSION['login']);
 
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
