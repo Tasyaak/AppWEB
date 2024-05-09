645aa78f860735d193916b6beca75e6cafa2439c
Oleg <61349805+Tasyaak@users.noreply.github.com>
2024-05-10 00:06:11 +0300

    Add files via upload

--- Zadacha4/database.php
+++ Zadacha4/database.php
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 <?php
     $db = new PDO('mysql:host=localhost;dbname=u67400', 'u67400', '9728892',
    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-=======
-<?php
-    $db = new PDO('mysql:host=localhost;dbname=u67400', 'u67400', '9728892',
-   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
->>>>>>> 40c49a915e65fe69917049f2091c09dd114c5c5f
 ?>
