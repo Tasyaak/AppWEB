df051b6c0bd0bfdf86a50c54a9f675b834d26128
Oleg <61349805+Tasyaak@users.noreply.github.com>
2024-05-24 20:03:15 +0300

    Add files via upload

--- Zadacha6/admin.php
+++ Zadacha6/admin.php
@@ -53,7 +53,7 @@ if (count($_POST)) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="bootstrap.min.css">
   <link rel="stylesheet" href="admin.css">
-  <title>Zadacha6 (admin)</title>
+  <title>Zadacha 6 (admin)</title>
 </head>
 
 <body>
